ea0b6a1ef052606f7d4c89cace60bb85e47f2fcd
Parth Zadeshwariya <parthzadeshwariya@gmail.com>
2025-12-25 16:30:11 +0530

    added tools to the llm

--- langgraphBackend.py
+++ langgraphBackend.py
@@ -4,6 +4,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from typing import TypedDict, Annotated
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
+import requests
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.tools import tool
+from langchain_tavily import TavilySearch
 from dotenv import load_dotenv
 import sqlite3
 
@@ -14,10 +19,63 @@ class ChatState(TypedDict):
 
 llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash')
 
+
+@tool
+def search_tool(query: str) -> list:
+    """
+    performs searching using Tavily Search"
+    """
+    search_tool = TavilySearch(
+        max_results=5,
+        topic="general"
+    )
+    result = search_tool.invoke({"query": query})
+    return result['results']
+
+@tool
+def calculator(first_number: float, second_number:float, operation: str) -> dict:
+    """
+    Performs basic arithmetic operations.
+    Supported operations: add, subtract, multiply, divide.
+    """
+    try:
+        if operation == "add":
+            result = first_number + second_number
+        elif operation == "subtract":
+            result = first_number - second_number
+        elif operation == "multiply":
+            result = first_number * second_number
+        elif operation == "divide":
+            if second_number == 0:
+                return {"error": "Division by zero is not allowed."}
+            result = first_number / second_number
+        else:
+            return {"error": "Invalid operation. Please use add, subtract, multiply, or divide."}
+        
+        return {"first_num": first_number, "second_num": second_number,"operation": operation, "result": result}
+    
+    except Exception as e:
+        return {"error": str(e)}
+    
+@tool
+def get_stock_price(symbol: str)-> dict:
+    """
+    Fetches the current stock price for a given symbol using AlphaVantage API.
+    """
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=S8MJZ48W30JLU2XE'  
+    response = requests.get(url)
+    return response.json()
+
+
+tools = [search_tool, calculator, get_stock_price]
+llm_with_tools = llm.bind_tools(tools)
+
+tool_node = ToolNode(tools)
+
 def chat_node(state: ChatState):
     messages = state['messages']
 
-    response = llm.invoke(messages)
+    response = llm_with_tools.invoke(messages)
 
     return {'messages': [response]}
 
@@ -28,8 +86,14 @@ graph = StateGraph(ChatState)
 
 graph.add_node('chat_node', chat_node)
 
-graph.add_edge(START, 'chat_node')
-graph.add_edge('chat_node', END)
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node", chat_node)
+graph.add_node("tools", tool_node)
+
+graph.add_edge(START, "chat_node")
+graph.add_conditional_edges("chat_node", tools_condition)
+graph.add_edge("tools", "chat_node")
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
